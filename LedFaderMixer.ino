6f93fe6efe457efcb25ca5f28100be9506cbc80d
ByteMasterFlinki <139759834+ByteMasterFlinki@users.noreply.github.com>
2024-01-04 00:08:28 +0100

    All Fader function, also the LEDs, comments are added

--- LedFaderMixer.ino
+++ LedFaderMixer.ino
@@ -1,57 +1,75 @@
 /*
-  Analog Input
+This is the code for a four fader LED controller.
+You have to adjust the Pins for your input and output devices.
+As an output I used a normal 5V LED Strip with commond Vcc (5V).
+For input I used fader how they are used in music mixer equipment.
+This code was designt for an Esp32-PICO-D4.
 
-  Demonstrates analog input by reading an analog sensor on analog pin 0 and
-  turning on and off a light emitting diode(LED) connected to digital pin 13.
-  The amount of time the LED will be on and off depends on the value obtained
-  by analogRead().
-
-  The circuit:
-  - potentiometer
-    center pin of the potentiometer to the analog input 0
-    one side pin (either one) to ground
-    the other side pin to +5V
-  - LED
-    anode (long leg) attached to digital output 13 through 220 ohm resistor
-    cathode (short leg) attached to ground
-
-  - Note: because most Arduinos have a built-in LED attached to pin 13 on the
-    board, the LED is optional.
-
-  created by David Cuartielles
-  modified 30 Aug 2011
-  By Tom Igoe
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogInput
+Designed by ByteMasterFlinki and his coworker SironLudwig 
 */
 
-int sensorPin = A0;   // select the input pin for the potentiometer
-int ledPin = D8;      // select the pin for the LED
-int sensorValue = 0;  // variable to store the value coming from the sensor
-int globalBrightnes = 0;
+#define DEBUG_OUTPUT true
+#define DEBUG_OUTPUT_SERIAL if(DEBUG_OUTPUT)Serial
+  // by changing 'true' to 'false' you can disable/enable serial output
 
+#define BrightnesFader 25
+#define RedFader       26
+#define GreenFader     32
+#define BlueFader      33
+  // define the input pins for your different faders/potentiometers
+
+#define RedLed   12
+#define GreenLed 13
+#define BlueLed  15
+  // define the output pins for the connected LEDs
+
+int Brightnes  = 0;
+int RedValue   = 0; 
+int GreenValue = 0;
+int BlueValue  = 0; 
+  // make variables for the value that get send to the output pins
+
+   
 void setup() {
 
   Serial.begin(9600);
-  // declare the ledPin as an OUTPUT:
-  pinMode(ledPin, OUTPUT);
+    // start the serial communication
+
+  while (!Serial);
+    // wait until Serial ist started
+    
+  pinMode(RedLed  , OUTPUT);
+  pinMode(GreenLed, OUTPUT);
+  pinMode(BlueLed , OUTPUT);
+    // set the pinmode to output
 }
+
 
 void loop() {
 
-  globalBrightnes = (sensorValue - 100 / 4)  ; // Blue+160, 
-  // read the value from the sensor:
-  sensorValue = analogRead(sensorPin);
-  // turn the ledPin on
-  analogWrite(ledPin, globalBrightnes);
-  Serial.print(globalBrightnes );
-  Serial.print(" ---- ");
-  Serial.println(sensorValue);
-  // stop the program for <sensorValue> milliseconds:
- delay(100);
+  Brightnes  = map(analogRead(BrightnesFader)  , -25, 4070, 0, 100);
+  RedValue   = (map(analogRead(RedFader)       , -25, 4070, 0, 255) * (-1 * Brightnes)) / 100 + 255;
+  GreenValue = (map(analogRead(GreenFader)     , -25, 4070, 0, 255) * (-1 * Brightnes)) / 100 + 255 ;
+  BlueValue  = (map(analogRead(BlueFader)      , -25, 4070, 0, 255) * (-1 * Brightnes)) / 100 + 255;
+    // calculating the output values depends on analog readings from the potentiometers.
+    // first the input from the input pins is calculatet to a number between zeroooo and 255.
+    // than the brightnes is also calculated in the variable
 
-  // stop the program for for <sensorValue> milliseconds:
+  analogWrite(RedLed  , RedValue);
+  analogWrite(GreenLed, GreenValue);
+  analogWrite(BlueLed , BlueValue);
+    // write the determined values to the LEDs
+
+  DEBUG_OUTPUT_SERIAL.print(RedValue);
+  DEBUG_OUTPUT_SERIAL.print(" ---- ");
+  DEBUG_OUTPUT_SERIAL.print(GreenValue);
+  DEBUG_OUTPUT_SERIAL.print(" ---- ");
+  DEBUG_OUTPUT_SERIAL.print(BlueValue);
+  DEBUG_OUTPUT_SERIAL.print(" ---- ");
+  DEBUG_OUTPUT_SERIAL.println(Brightnes);
+    // serial output for debuging (disable/enablen with #define DEBUG_OUTPUT 'true' or 'false')
+  
+ delay(50);
+  // let the program wait for 50 milliseconds
 
 }
